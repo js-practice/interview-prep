aac4e00ea9790587a23b2beb11ec8010cf27b53a
Karim H Kyler <karim.kyler@gmail.com>
2015-04-01 18:19:40 -0700

    added perfect squares and reverse polish notation

---
+++ perfectSquare/perfectSquare.js
@@ -0,0 +1,14 @@
+/**
+ * This function takes an argument that is a number and
+ * returns true if that number is a perfect square.
+ *
+ * Examples:
+ *  perfectSquareFinder(25); //true
+ *  perfectSquareFinder(26); //false
+ *
+ *
+ */
+
+var perfectSquareFinder = function(num){
+  //TODO implement solution
+};
---
+++ postfixCalculator/postfixCalculator.js
@@ -0,0 +1,22 @@
+/**
+ * This function takes an argument that is a string
+ * and returns the number value of the arguments with the operator afterward.
+ *
+ * Reverse Polish notation (RPN) is a mathematical notation in which every
+ * operator follows all of its operands, in contrast to Polish notation, which
+ * puts the operator in the prefix position. It is also known as postfix
+ * notation and is parenthesis-free as long as operator arities are fixed. The
+ * description "Polish" refers to the nationality of logician Jan Łukasiewicz,
+ * who invented (prefix) Polish notation in the 1920s.
+ *
+ * Examples:
+ *  postfixCalculator('1 2 3 + +');      //6
+ *  postfixCalculator('13 2 4 * 5 + -'); //0
+ *  postfixCalculator('7 3 + 20 /');    //0.5
+ *
+ *
+ */
+
+var postfixCalculator = function(){
+  // TODO: implement solution
+};
