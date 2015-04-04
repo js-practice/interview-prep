bbc021bbf39adc0ab67a86b580dcfdeae9cc6f93
Karim H Kyler <karim.kyler@gmail.com>
2015-04-04 15:15:34 -0700

    added jack and jill and string string

---
+++ jackAndJill/jackAndJill.js
@@ -0,0 +1,14 @@
+/**
+ * Write a function that takes an array of names and outputs an array where Jack is
+ * always first and Jill comes second followed by the rest of the names.
+ *
+ * Examples:
+ * rollOrder(['bill', 'sam', 'jill', 'joe', 'jack']) -> ['jack', 'jill', 'bill', 'sam', 'joe']
+ * rollOrder(['jill', 'jack', 'bill', 'jack']) -> ['jack', 'jack', 'jill', 'bill']
+ * rollOrder(['john', 'bill', 'sam', 'jill']) -> ['jill', 'john', 'bill', 'sam']
+ * rollOrder(['john', 'bill', 'sam']) -> ['john', 'bill', 'sam']
+ */
+
+ var rollOrder = function(){
+   // TODO: implement solution
+ }
---
+++ strString/strStr.js
@@ -0,0 +1,18 @@
+/*
+StrStr
+
+Input:
+    baseString, patternString
+
+Output:
+    The first index in baseString where patternString occurs, or -1 if it does not
+
+
+strStr('pineapple','apple')  -> 4
+strStr('pineapple','orange') -> -1
+strStr('pineapple','e')      -> 3
+*/
+
+var strStr =function(baseString, patternString){
+  // TODO: implement solution
+};
